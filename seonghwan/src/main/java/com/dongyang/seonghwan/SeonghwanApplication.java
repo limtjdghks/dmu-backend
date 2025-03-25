9af0456a9e976c3e23c72eb0a24fe86fd62ff8fe
limtjdghks <epaktldk0203@gmail.com>
2025-03-25 12:34:11 +0900

    2025년 3월 25일 실습

---
+++ seonghwan/src/main/java/com/dongyang/seonghwan/SeonghwanApplication.java
@@ -0,0 +1,13 @@
+package com.dongyang.seonghwan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeonghwanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeonghwanApplication.class, args);
+	}
+
+}
---
+++ seonghwan/src/test/java/com/dongyang/seonghwan/SeonghwanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongyang.seonghwan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeonghwanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
